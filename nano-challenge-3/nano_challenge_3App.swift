c501d402b557620e753f36b7c437b536cc3a4dab
eric <ericfrndo@gmail.com>
2024-07-11 16:58:03 +0700

    Initial Commit

---
+++ nano-challenge-3/nano_challenge_3App.swift
@@ -0,0 +1,17 @@
+//
+//  nano_challenge_3App.swift
+//  nano-challenge-3
+//
+//  Created by Eric Fernando on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct nano_challenge_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
