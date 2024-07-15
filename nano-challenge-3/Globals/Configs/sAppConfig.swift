fb76ab4473f83085633df3f9463e9f19803f4235
Vinn <kevgun03@gmail.com>
2024-07-15 19:47:56 +0700

    Added extensions and Globals

---
+++ nano-challenge-3/Globals/Configs/sAppConfig.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct AppConfig {
+    
+    static let debugMode : Bool = true
+    
+    static let delimiter : String = "."
+    static let defaultFilename : String = "Untitled"
+    static let defaultFoldername : String = "Untitled"
+    
+}
